abdf45d7c9e364ceb2806a71bb363d1857c95a38
Sven Neumann <sven@svenfoo.org>
2020-12-10 16:49:16 +0100

    Use re-exported pins
    This depends on a change in avr-hal.

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,19 @@ use board::prelude::*;
 fn main() -> ! {
     let dp = board::Peripherals::take().unwrap();
 
-    let portb = dp.PORTB.split();
-    let mut tx0 = portb.pb4.into_output(&portb.ddr);
-    let mut rx0 = portb.pb5.into_output(&portb.ddr);
+    let mut pins = board::Pins::new(dp.PORTA, dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF);
 
-    // Digital pin 13 is connected to the onboard LED
-    // let mut pins = board::Pins::new(dp.PORTA, dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF);
-    // let mut led = pins.d13.into_output(&mut pins.ddr);
+    let mut led = pins.d13.into_output(&mut pins.ddr);
+    let mut tx = pins.led_tx.into_output(&mut pins.ddr);
+    let mut rx = pins.led_rx.into_output(&mut pins.ddr);
 
-    tx0.set_high().void_unwrap();
-    rx0.set_low().void_unwrap();
+    led.set_high().void_unwrap();
+    tx.set_high().void_unwrap();
+    rx.set_low().void_unwrap();
 
     loop {
-        tx0.toggle().void_unwrap();
-        rx0.toggle().void_unwrap();
+        tx.toggle().void_unwrap();
+        rx.toggle().void_unwrap();
 
         board::delay_ms(100);
     }
