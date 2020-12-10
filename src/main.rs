ee8f551594e74b995e50562d74402388fbb70110
Sven Neumann <sven@svenfoo.org>
2020-12-10 13:44:12 +0100

    Play with the TX/RX LEDs instead of the onboard one

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,22 @@ use board::prelude::*;
 #[board::entry]
 fn main() -> ! {
     let dp = board::Peripherals::take().unwrap();
-    let mut pins = board::Pins::new(dp.PORTA, dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF);
 
-    // Digital pin 13 is also connected to an onboard LED marked "L"
-    let mut led = pins.d13.into_output(&mut pins.ddr);
+    let portb = dp.PORTB.split();
+    let mut tx0 = portb.pb4.into_output(&portb.ddr);
+    let mut rx0 = portb.pb5.into_output(&portb.ddr);
 
-    led.set_high().void_unwrap();
+    // Digital pin 13 is connected to the onboard LED
+    // let mut pins = board::Pins::new(dp.PORTA, dp.PORTB, dp.PORTC, dp.PORTD, dp.PORTE, dp.PORTF);
+    // let mut led = pins.d13.into_output(&mut pins.ddr);
+
+    tx0.set_high().void_unwrap();
+    rx0.set_low().void_unwrap();
 
     loop {
-        led.toggle().void_unwrap();
+        tx0.toggle().void_unwrap();
+        rx0.toggle().void_unwrap();
+
         board::delay_ms(100);
     }
 }
-
